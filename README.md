cc364d0acce8a978f801ef455057a5dec6708b45
sanjithbonela <sanjithbonela@gmail.com>
2021-11-24 12:32:56 +0100

    working version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # IIS_Project
+
+In this part of the project, we implement the landmarks detection of ASL Videos using ML/DL architectures. The project consists of three phases and one optional phase: landmarks detection, gesture recognition, and the appropriate response of the gesture by Furhat Robot and with optional being End-to-End Learning.
+
+This the first part of final project is in Intelligent Interactive Systems course at Uppsala University.
+
+Input - An anonymous video of ASL.
+
+Expected Output -  X and Y coordinates of landmarks for every frame.
+
+#Directory Structure
+Folder structure of videos and annotations dataset - Default ("../final_project_dataset/*")
+
+Folder structure of imgs dataset - "../final_project_dataset_v0/imgs"
+
+#Libraries & Utilities
+Pytorch, pretrained Resnet18 model, MSE Loss, implemented on GPU - RTX-2060.
+
+#Usage
+1. Convert all videos into images using function implemented in dataparser.py
+2. Run train.py which includes preprocessing (to avoid overfitting/underfitting issues).
+
+Good Luck!!
---
+++ test.py
@@ -0,0 +1,45 @@
+"""TODO!"""
+
+import sys
+import time
+import numpy as np
+import torch
+import cv2
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset
+from LandmarksDataset import FaceLandmarksDataset
+from Transforms import Transforms
+from network_model import Network
+from network_model import CustomNet
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    images = cv2.imread("../final_project_dataset_v0/test_imgs/test_set/test_img.jpg", 0)
+    images = torch.from_numpy(images)
+    print(images.shape)
+
+    with torch.no_grad():
+        best_network = Network()
+        best_network.cuda()
+        best_network.load_state_dict(torch.load('./content/landmarks_pretrained.pth'))
+        best_network.eval()
+
+        predictions = (best_network(images).cpu() + 0.5)
+        predictions = predictions.view(-1, 20, 2)
+
+        plt.figure(figsize=(10, 40))
+
+        for img_num in range(8):
+            plt.subplot(8, 1, img_num + 1)
+            imgplot = plt.imshow(images[img_num].cpu().numpy().transpose(1, 2, 0).squeeze(), cmap='gray')
+            plt.scatter(predictions[img_num, :, 0], predictions[img_num, :, 1], c='r', s=5)
+            plt.show()
+
+    print('Total number of test images: {}'.format(1))
+
+    end_time = time.time()
+    print("Elapsed Time : {}".format(end_time - start_time))
