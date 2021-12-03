0cc7b4ceebbf5813b1832f132643ec1540c954b4
sanjithbonela <sanjithbonela@gmail.com>
2021-12-03 21:22:29 +0100

    renamed

--- pythonProject3/src/kafka_producer.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaProducer
-
-if __name__ == '__main__':
-    producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-    #producer.send('my_topic', bytes('Hello!', 'utf-8'))
-    ack = producer.send(topic='my_topic', value=bytes('Hello!', 'utf-8'))
-    print(ack.get())
